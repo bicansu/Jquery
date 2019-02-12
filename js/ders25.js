5bbc0a5e96514206386e2d3c794569aa4c22c023
Cansu Bican <cansubican11@gmail.com>
2019-02-12 12:19:21 +0300

    Submit Fonksiyonu

---
+++ js/ders25.js
@@ -0,0 +1,4 @@
+$('#uyelik_formu').submit(function(){
+    var email_deger = $('#email_deger').val();
+    $('#sonuc').html('Teşekkürler! '+ email_deger + ' adresiniz alındı!');
+});
---
+++ submit.php
@@ -0,0 +1,20 @@
+<html lang="tr">
+<head>
+    <meta charset="uft-8">
+    <title>Jquery Change Özelliği </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    
+    <form action="#" id="uyelik_formu">
+        <p>Email: <input type="email" id="email_deger" /></p>
+        <p><input type="submit" value ="Kayit Ol"></p>
+    </form>
+
+    <span id="sonuc"></span>
+
+
+    <script type="text/javascript"  src="js/jquery.js"></script>
+    <script type="text/javascript"  src="js/ders25.js"></script>
+</body>
+</html>    
