abb1c156c478dbce082b01d202b8271fa6762e80
Cansu Bican <cansubican11@gmail.com>
2019-02-12 14:57:39 +0300

    bind

--- js/ders31.js
+++ js/ders31.js
@@ -1,9 +1,16 @@
 //Bind::Birden fazla methodun jqueryde kullanılmasını sağlayan fonskiyondur.
-$('a').mouseenter(function(){
-    $(this).addClass('bold');
-}).mouseleave(function(){
-    $(this).removeClass('bold');
-}); 
+// $('a').mouseenter(function(){
+//     $(this).addClass('bold');
+// }).mouseleave(function(){
+//     $(this).removeClass('bold');
+// }); 
 
 
 //mouseenter üzerine gelinen yere istediğimiz özelliği yapmamızı sağlar.
+//İkisini Birlikte Kullanmak İçim
+
+$('a').bind('mouseenter mouseleave', function(){
+    $(this).toggleClass('bold');
+
+});
+//ToggleClass bir classı uygular ve kaldırır.
