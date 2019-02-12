32093980ca61a3df8f4bc744e97da56cb7cb58d8
Cansu Bican <cansubican11@gmail.com>
2019-02-12 14:28:11 +0300

    select

---
+++ js/ders28.js
@@ -0,0 +1,6 @@
+$(':input').select(function(){
+    $('span').html('Bir şeyler seçildi').show().fadeOut(1000);
+
+});
+//fadeOut dediğinizde 1000 sc içerisinde gösterdiği şeyi kaldırır.
+//yani 1sn içerisinde yazı kaybolur.
---
+++ select.php
@@ -0,0 +1,17 @@
+<html lang="tr">
+<head>
+    <meta charset="uft-8">
+    <title>Jquery Select Özelliği </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="//code.jquery.com/jquery=1.10.2.js"></script>
+    
+</head>
+<body>
+    
+    <input id="birtext" type="text">
+    <span id="sonuc"></span>
+
+    <script type="text/javascript"  src="js/jquery.js"></script>
+    <script type="text/javascript"  src="js/ders28.js"></script>
+</body>
+</html>    
