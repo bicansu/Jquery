5c1685faa7afea211d8c0ecf8fa77f65a9e7eee7
Cansu Bican <cansubican11@gmail.com>
2019-02-12 14:06:36 +0300

    hover

---
+++ hover.php
@@ -0,0 +1,20 @@
+<html lang="tr">
+<head>
+    <meta charset="uft-8">
+    <title>Jquery Change Özelliği </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    
+    <a href="#" id="menu_php"> Php </a>/
+    <a href="#" id="menu_css"> Css </a>/
+    <a href="#" id="menu_jquery"> Jquery </a>
+
+    <span id="secim">
+    
+    </span>
+
+    <script type="text/javascript"  src="js/jquery.js"></script>
+    <script type="text/javascript"  src="js/ders27.js"></script>
+</body>
+</html>    
---
+++ js/ders27.js
@@ -0,0 +1,12 @@
+$('#menu_php').hover(function(){
+    $('#secim').html('Php derslerini seçtiniz!');
+});
+//istediğimiz linkin üzerine gelir gelmez 
+//ona ait spandaki yazı gelir.
+$('#menu_css').hover(function(){
+    $('#secim').html('Css derslerini seçtiniz!');
+});
+
+$('#menu_jquery').hover(function(){
+    $('#secim').html('Jquery derslerini seçtiniz!');
+});
