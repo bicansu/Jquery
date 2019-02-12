30a9f2884e4ebdbc2d808842eb41bfdbc020cba2
Cansu Bican <cansubican11@gmail.com>
2019-02-12 12:42:26 +0300

    Toggle

---
+++ js/ders26.js
@@ -0,0 +1,6 @@
+$('#link').click(function(){
+$('#sonuc').toggle();
+});
+
+
+//Toggle özelliği iki fonksiyonu iç içe olucak şekilde oluşturabiliriz.
---
+++ toggle.php
@@ -0,0 +1,19 @@
+<html lang="tr">
+<head>
+    <meta charset="uft-8">
+    <title>Jquery Toggle Özelliği </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    
+    <a href="#" id="link">Tıkla</a>
+    <?/*Toglle özelliği ilk tıkladığımızda farkli
+    ikinci kez tıkladığımızda farklı sonuç getirmesidirç*/?>
+    <span id="sonuc"> 
+     Cansu 
+    </span>
+
+    <script type="text/javascript"  src="js/jquery.js"></script>
+    <script type="text/javascript"  src="js/ders26.js"></script>
+</body>
+</html>    
