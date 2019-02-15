89bc8b9c0e3acd9df3a4b25008399dab0c7b7758
Cansu Bican <cansubican11@gmail.com>
2019-02-15 11:20:17 +0300

    ToggleClass

---
+++ ToggleClass.php
@@ -0,0 +1,17 @@
+ <html lang="tr">
+<head>
+    <meta charset="uft-8">
+    <title>Jquery Select Özelliği </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="//code.jquery.com/jquery=1.10.2.js"></script>
+    
+</head>
+<body>
+    
+    <input id="button" type="button" value="Button" />
+    <p id="text">Cansu Bican</p>
+
+    <script type="text/javascript"  src="js/jquery.js"></script>
+    <script type="text/javascript"  src="js/ders40.js"></script>
+</body>
+</html>    
---
+++ js/ders40.js
@@ -0,0 +1,4 @@
+//ToggleClass ile remove add özelikklerini aynı anda kullanabiliriz.
+$('#button').click(function(){
+    $('#text').toggleClass('bold arkaPlan');
+});
