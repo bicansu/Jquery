1c57070d024f9e77f226e240b4f741db4ec88890
Cansu Bican <cansubican11@gmail.com>
2019-02-15 11:09:17 +0300

    removeClass

---
+++ js/ders39.js
@@ -0,0 +1,8 @@
+//RemoveClass İstediğimi bölümdeki css özelliğini kaldıramamızı sağlar.
+$('#ekle').click(function(){
+    $('#text').addClass('bold');
+});
+
+$('#sil').click(function(){
+    $('#text').removeClass('bold');
+});
---
+++ removeClass.php
@@ -0,0 +1,19 @@
+<html lang="tr">
+<head>
+    <meta charset="uft-8">
+    <title>Jquery Select Özelliği </title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <script src="//code.jquery.com/jquery=1.10.2.js"></script>
+    
+</head>
+<body>
+     <input id="ekle" type="button" value="Ekle">
+     <input id="sil" type="button" value="Sil">
+
+     <p id="text">Cansu Bican</p>
+      
+
+    <script type="text/javascript"  src="js/jquery.js"></script>
+    <script type="text/javascript"  src="js/ders39.js"></script>
+</body>
+</html>    
